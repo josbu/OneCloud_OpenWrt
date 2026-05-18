35ddcb89b1d3f668c5269ad11a9ad9f95d43b827
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-05-19 03:24:26 +0800

    Update setup.sh

--- sh/setup.sh
+++ sh/setup.sh
@@ -3,9 +3,12 @@ source $GITHUB_WORKSPACE/sh/functions.sh
 
 merge_package master https://github.com/coolsnowwolf/packages package lang/rust
 #git clone https://github.com/stupidloud/helloworld package/helloworld
+merge_package v5 https://github.com/sbwml/luci-app-mosdns package v2dat
+
 
 rm -rf package/helloworld/v2ray-geodata
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+#https://github.com/sbwml/luci-app-mosdns
 
 merge_package main https://github.com/kiddin9/op-packages package shadowsocks-rust
 merge_package main https://github.com/kiddin9/op-packages package shadowsocks-libev
@@ -15,7 +18,7 @@ merge_package main https://github.com/kiddin9/op-packages package luci-app-ssr-p
 merge_package main https://github.com/kiddin9/op-packages package xray-core
 merge_package main https://github.com/kiddin9/op-packages package luci-app-mosdns
 merge_package main https://github.com/kiddin9/op-packages package mosdns
-merge_package main https://github.com/kiddin9/op-packages package v2dat
+#merge_package main https://github.com/kiddin9/op-packages package v2dat
 
 rm -rf feeds/luci/applications/luci-app-homeproxy
 git clone https://github.com/VIKINGYFY/homeproxy package/luci-app-homeproxy
