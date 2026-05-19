d4bd5f2154896ceccefb9bbbefc29b32bca4eb77
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-05-20 04:04:18 +0800

    Update lean2.sh

--- sh/lean2.sh
+++ sh/lean2.sh
@@ -10,7 +10,8 @@ sed -i "s/hostname='.*'/hostname='$OWRT_NAME'/g" ./package/base-files/files/bin/
 sed -i "s/timezone='.*'/timezone='CST-8'/g" ./package/base-files/files/bin/config_generate
 sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" ./package/base-files/files/bin/config_generate
 
-rm -rf feeds/packages/net/sing-box
+#最新golang
+rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
 
 # Modify default NTP server
