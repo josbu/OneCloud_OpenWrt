53da94cfe3b7df8f45335857a94148d84a8879e8
莫小小 <67265640+rmoyulong@users.noreply.github.com>
2026-05-20 16:34:49 +0800

    Delete sh/python3.patch.sh

--- sh/python3.patch.sh
+++
@@ -1,7 +0,0 @@
-#公用函数
-source $GITHUB_WORKSPACE/sh/functions.sh
-
-cd openwrt
-merge_package master https://github.com/rmoyulong/old_coolsnowwolf_packages package lang/python
-rm -rf feeds/packages/lang/python
-cp -rf ./package/python feeds/packages/lang/
